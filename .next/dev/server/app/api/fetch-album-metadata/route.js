9b2280dce9247ca744edf752b7fa2c7b897b4dd4
David Thomas <davidthomas411@gmail.com>
2025-11-18 16:24:16 -0500

    Stop tracking .next build artifacts

--- .next/dev/server/app/api/fetch-album-metadata/route.js
+++
@@ -1,6 +0,0 @@
-var R=require("../../../chunks/[turbopack]_runtime.js")("server/app/api/fetch-album-metadata/route.js")
-R.c("server/chunks/275cd_next_1cb2c630._.js")
-R.c("server/chunks/[root-of-the-server]__2ff68514._.js")
-R.c("server/chunks/_next-internal_server_app_api_fetch-album-metadata_route_actions_11e1172b.js")
-R.m("[project]/node_modules/.pnpm/next@16.0.3_react-dom@19.2.0_react@19.2.0__react@19.2.0/node_modules/next/dist/esm/build/templates/app-route.js { INNER_APP_ROUTE => \"[project]/app/api/fetch-album-metadata/route.ts [app-route] (ecmascript)\" } [app-route] (ecmascript)")
-module.exports=R.m("[project]/node_modules/.pnpm/next@16.0.3_react-dom@19.2.0_react@19.2.0__react@19.2.0/node_modules/next/dist/esm/build/templates/app-route.js { INNER_APP_ROUTE => \"[project]/app/api/fetch-album-metadata/route.ts [app-route] (ecmascript)\" } [app-route] (ecmascript)").exports
--- .next/dev/server/chunks/_next-internal_server_app_api_fetch-album-metadata_route_actions_11e1172b.js
+++
@@ -1,7 +0,0 @@
-module.exports = [
-"[project]/.next-internal/server/app/api/fetch-album-metadata/route/actions.js [app-rsc] (server actions loader, ecmascript)", ((__turbopack_context__, module, exports) => {
-
-}),
-];
-
-//# sourceMappingURL=_next-internal_server_app_api_fetch-album-metadata_route_actions_11e1172b.js.map
